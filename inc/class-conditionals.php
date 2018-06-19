8c5c3e8340f2d0b3364a95acabf987ba1e3fd6d7
Jim <jim.appelt@gmail.com>
2018-06-19 19:43:13 +0200

    Init conditional class

---
+++ inc/class-conditionals.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Conditionals.
+ *
+ * @package Fl_WAPL_Bridge
+ */
+
+namespace Flwapl\Inc;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Conditionals
+ */
+class Conditionals {
+
+	/**
+	 * Check if Flatsome theme is activated.
+	 *
+	 * @return bool
+	 */
+	public function is_flatsome_activated() {
+		$theme   = wp_get_theme( get_template() );
+		$name    = $theme->get( 'Name' );
+
+		return 'Flatsome' === $name;
+	}
+}
